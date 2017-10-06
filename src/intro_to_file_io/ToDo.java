0a8c684bc167dd4f8c8751d15fd2897a62b65bef
eshaank <league@WTS-iMac-11.attlocal.net>
2017-10-05 17:03:41 -0700

    finish ToDo stuff
    remove thingy doesn’t work

--- src/intro_to_file_io/ToDo.java
+++ src/intro_to_file_io/ToDo.java
@@ -2,9 +2,8 @@ package intro_to_file_io;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -20,13 +19,17 @@ public class ToDo implements ActionListener {
 	// 4. Make a to-do list tracker:
 	// Create a window with four buttons: add task, remove task, save, and load.
 	// (Done)
-	// The add task button will display a JOptionPane to get a task from the user
+	// The add task button will display a JOptionPane to get a task from the
+	// user
 	// and add it to the list.
-	// Remove task will remove a task from the list. You can choose your own method
+	// Remove task will remove a task from the list. You can choose your own
+	// method
 	// of how to select a task from the list.
 	// The save button will save the current task list to a file.
-	// The load button will load a new task list into the program and display it.
-	// The program should automatically load the last saved list from the file at
+	// The load button will load a new task list into the program and display
+	// it.
+	// The program should automatically load the last saved list from the file
+	// at
 	// startup and display it.
 
 	JFrame frame;
@@ -35,6 +38,8 @@ public class ToDo implements ActionListener {
 	JButton removeTask;
 	JButton save;
 	JButton load;
+
+	String file;
 
 	PrintWriter pw;
 	Scanner scan;
@@ -84,46 +89,43 @@ public class ToDo implements ActionListener {
 		if (e.getSource() == addTask) {
 
 			String task = JOptionPane.showInputDialog(null, "Type a task to be done");
-			list.add(task);
-			
-			
+			list.add(task + "\n");
+			task = "";
 
-				try {
-					for (int i = 0; i < list.size(); i++) {
+			try {
+				for (int i = 0; i < list.size(); i++) {
 					FileWriter fw = new FileWriter("src/intro_to_file_io/List.txt", true);
 					fw.write(list.get(i));
 
 					fw.close();
-					}
-				} catch (IOException e1) {
-					e1.printStackTrace();
 				}
+			} catch (IOException e1) {
+				e1.printStackTrace();
 			}
-		
+		}
 
 		if (e.getSource() == load) {
 
+			file = "";
 			try {
-				BufferedReader br = new BufferedReader(new FileReader("src/intro_to_file_io/List.txt"));
+				scan = new Scanner(new File("src/intro_to_file_io/List.txt"));
+				while (scan.hasNextLine()) {
 
-				String line = br.readLine();
-				while (line != null) {
-			JOptionPane.showMessageDialog(null, line);
-					line = br.readLine();
+					file += scan.nextLine();
+					file += "\n";
 				}
+				JOptionPane.showMessageDialog(null, file);
 
-				br.close();
 			} catch (FileNotFoundException e1) {
+
 				e1.printStackTrace();
-			} catch (IOException e2) {
-				e2.printStackTrace();
 			}
+
 		}
-
 		if (e.getSource() == removeTask) {
-			String remove = JOptionPane.showInputDialog("Which task would you like to remove? \n" + list);
-
+			String remove = JOptionPane.showInputDialog("Which task would you like to remove? \n" + file);
 		}
 
 	}
+
 }
